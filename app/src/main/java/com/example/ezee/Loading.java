15e9163d1daff7cf250ff54173b8bd03bf8eccd3
GautamChaurasia <gautamchaurasia501@gmail.com>
2022-03-31 14:43:19 +0530

    Notification History Layout updated

--- app/src/main/java/com/example/ezee/Loading.java
+++ app/src/main/java/com/example/ezee/Loading.java
@@ -9,27 +9,11 @@ import androidx.fragment.app.Fragment;
 public class Loading {
 
     Activity activity;
-    Fragment fragment;
     AlertDialog dialog;
 
     public Loading(Activity myActivity) {
         activity = myActivity;
     }
-
-//        public Loading(Fragment myfragment){
-//        fragment = myfragment;
-//    }
-
-//    void startLoadingfragment(){
-//        AlertDialog.Builder builder;
-//        builder = new AlertDialog.Builder(fragment);
-//        LayoutInflater inflater = fragment.getLayoutInflater();
-//        builder.setView(inflater.inflate(R.layout.loading_spinner, null));
-//        builder.setCancelable(false);
-//
-//        dialog = builder.create();
-//        dialog.show();
-//    }
 
     void startLoading(){
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
