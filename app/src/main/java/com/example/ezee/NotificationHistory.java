df6ac89df0ff2b725b63b8b7e1a5f3960f617ada
GautamChaurasia <gautamchaurasia501@gmail.com>
2022-03-31 16:21:06 +0530

    improvements in Notification History

--- app/src/main/java/com/example/ezee/NotificationHistory.java
+++ app/src/main/java/com/example/ezee/NotificationHistory.java
@@ -17,6 +17,7 @@ public class NotificationHistory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_history);
+        getSupportActionBar().setTitle("Notifications");
 
         NotiHist = (RecyclerView) findViewById(R.id.NotiHistRclV);
         NotiHist.setLayoutManager(new LinearLayoutManager(this));
